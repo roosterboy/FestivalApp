458644c9779100573612b9375caa0b3f2858471e
roosterboy <pat_wynne@roosterville.net>
2021-04-27 01:22:23 -0700

    Initial Commit

---
+++ FestivalApp/FestivalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FestivalAppApp.swift
+//  FestivalApp
+//
+//  Created by Patrick Wynne on 4/27/21.
+//
+
+import SwiftUI
+
+@main
+struct FestivalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
